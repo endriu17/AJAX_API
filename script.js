bd6004ea78471fd41da33d478c03520dfc7aa96b
endriu17 <aolaszewski@gmail.com>
2018-08-29 16:17:20 +0200

    add special features and style

--- script.js
+++ script.js
@@ -17,7 +17,14 @@ function searchCountries() {
 function showCountriesList(resp) {
     countriesList.empty();
     resp.forEach(function (item) {
-        $('<li>').text(item.name).appendTo(countriesList);
+        $('<li>').html('<div class="header"><img src="'+item.flag+'">' + '<h2>' + item.name + '</h2></div>').appendTo(countriesList);
+        $('<li>').html('<tr><th> Background information: </th></tr>').appendTo(countriesList);
+        $('<li>').html('<tr><td> Capital </td>' + '<td> :  &nbsp' + item.capital + '</td></tr>').appendTo(countriesList);
+        $('<li>').html('<tr><td> Land area </td>' + '<td> :  &nbsp' + item.area + '</td></tr>').appendTo(countriesList);
+        $('<li>').html('<tr><td> Population </td>' + '<td> :  &nbsp' + item.population + '</td></tr>').appendTo(countriesList);
+        $('<li>').html('<tr><td> Language(s) </td>' + '<td> :  &nbsp' + item.languages[0].name + '</td></tr>').appendTo(countriesList);
+        $('<li>').html('<tr><td> Currency </td>' + '<td> :  &nbsp' + item.currencies[0].name + ' ' + '(' + item.currencies[0].symbol + ')' + '</td></tr>').appendTo(countriesList);
+        $('<li>').html('<tr><th></th></tr>').appendTo(countriesList);
     });
 }
 
